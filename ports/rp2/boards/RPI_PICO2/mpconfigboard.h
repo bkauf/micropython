807d983f8594e66dca17de30891790e46e59edef
Brian <brian.kaufman@gmail.com>
2026-04-27 15:45:38 -0400

    adjusted flash memory to 1MB for firmware

--- ports/rp2/boards/RPI_PICO2/mpconfigboard.h
+++ ports/rp2/boards/RPI_PICO2/mpconfigboard.h
@@ -1,3 +1,6 @@
 // Board and hardware specific configuration
-#define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico2"
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - 1024 * 1024)
+#define MICROPY_HW_BOARD_NAME "Raspberry Pi Pico2"
+// #define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - 1024
+// * 1024)
+//  Find this line and change 3MB to 1MB:
+#define MICROPY_HW_FLASH_STORAGE_BYTES (1 * 1024 * 1024)
